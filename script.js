06968c7b267f386759b88e9888140f5f4640d334
Kwshal <kushal3738@gmail.com>
2024-08-24 11:47:34 +0530

    sup

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-var hi;
+
+
+function findRoots() {
+        var a = document.getElementById("a").value;
+        var b = document.getElementById("b").value;
+        var c = document.getElementById("c").value;
+        // var d = Math.sqrt(b**2 + 
+}
