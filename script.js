cdbd689ee9f38a60e2c0bab07dfb398a96e79fcf
Kwshal <kushal3738@gmail.com>
2024-08-24 12:38:08 +0530

    hi

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-
-
 function findRoots() {
-        var a = document.getElementById("a").value;
-        var b = document.getElementById("b").value;
-        var c = document.getElementById("c").value;
-        // var d = Math.sqrt(b**2 + 
+  var a = Number(document.getElementById("a").value);
+  var b = Number(document.getElementById("b").value);
+  var c = Number(document.getElementById("c").value);
+
+  var d = Math.sqrt(b ** 2 - 4 * a * c);
+  var root1 = (-b + d) / (2 * a);
+  var root2 = (-b - d) / (2 * a);
+
+
+  if (d < 0) {
+    document.getElementById("root1").innerHTML = "No real roots.";
+    document.getElementById("root2").innerHTML = "No real roots.";
+    return;
+  } else if (a != "" && b != "" && c != "") {
+    document.getElementById("root1").innerHTML = root1;
+    document.getElementById("root2").innerHTML = root2;
+  }
 }
